AOKP/packages_apps_ROMControl

Add internal su queue; add fcharge state changer receiver
see ExternalCommandService class description

Change-Id: Ib96b3ee516979d69383fa35689321335fe4ae95d
Signed-off-by: Roman Birg <romanbirg@gmail.com>

src/com/aokp/romcontrol/service/ChangeFastChargeStateReceiver.java
@@ -1,0 +1,25 @@
+
+package com.aokp.romcontrol.service;
+
+import java.io.File;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String fCHargePath = context
+                .getString(com.android.internal.R.string.config_fastChargePath);
+        if (fCHargePath == null || fCHargePath.isEmpty() || !new File(fCHargePath).exists()) {
+            Log.e("ROMControl", "Attempted to change fast charge state but it's not enabled?");
+        }
+        final String value = intent.getBooleanExtra("newState", false) ? "1" : "0";
+        context.startService(new Intent(context,
+                ExternalCommandService.class)
+                .putExtra("cmd", "echo " + value + " > " + fCHargePath));
+    }
+}

src/com/aokp/romcontrol/service/ExternalCommandService.java
@@ -1,0 +1,27 @@
+
+package com.aokp.romcontrol.service;
+
+import com.aokp.romcontrol.util.CMDProcessor;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Helper class to queue su commands that can be run from broadcast receivers,
+ * specifically like ones defined in AndroidManifest.xml, as they are run in the
+ * main thread, and starting async operations does not guarantee execution. This
+ * class should not be accessible from outside ROMControl. Commands will be queued.
+ */
+public class ExternalCommandService extends IntentService {
+
+    public ExternalCommandService() {
+        super(ExternalCommandService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent.hasExtra("cmd")) {
+            new CMDProcessor().su.runWaitFor(intent.getStringExtra("cmd"));
+        }
+    }
+}

succulent/packages_apps_XYSettings

src/com/xylon/settings/service/ChangeFastChargeStateReceiver.java
@@ -1,0 +1,26 @@
+
+package com.xylon.settings.service;
+
+import java.io.File;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ChangeFastChargeStateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String fCHargePath = context
+                .getString(com.android.internal.R.string.config_fastChargePath);
+        if (fCHargePath == null || fCHargePath.isEmpty() || !new File(fCHargePath).exists()) {
+            Log.e("ROMControl", "Attempted to change fast charge state but it's not enabled?");
+            return;
+        }
+        final String value = intent.getBooleanExtra("newState", false) ? "1" : "0";
+        context.startService(new Intent(context,
+                ExternalCommandService.class)
+                .putExtra("cmd", "echo " + value + " > " + fCHargePath));
+    }
+}

src/com/xylon/settings/service/ExternalCommandService.java
@@ -1,0 +1,27 @@
+
+package com.xylon.settings.service;
+
+import com.xylon.settings.util.CMDProcessor;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Helper class to queue su commands that can be run from broadcast receivers,
+ * specifically like ones defined in AndroidManifest.xml, as they are run in the
+ * main thread, and starting async operations does not guarantee execution. This
+ * class should not be accessible from outside ROMControl. Commands will be queued.
+ */
+public class ExternalCommandService extends IntentService {
+
+    public ExternalCommandService() {
+        super(ExternalCommandService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent.hasExtra("cmd")) {
+            new CMDProcessor().su.runWaitFor(intent.getStringExtra("cmd"));
+        }
+    }
+}