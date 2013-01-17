eeae5677cd9cddde0d68b45408b6718b1addeae6
Daniel <daxx4182@gmail.com>
2013-01-17 22:03:36 +0800

    Navbar Hide (Is still a WIP feature, a beta feature)
    A seperate. Just commit this to XYE, it's wasn't meant to be used everyday.

--- src/com/xylon/settings/fragments/NavigationBar.java
+++ src/com/xylon/settings/fragments/NavigationBar.java
@@ -86,6 +86,8 @@ public class NavigationBar extends SettingsPreferenceFragment implements
     private static final String ENABLE_NAVRING_LONG = "enable_navring_long";
     private static final String NAVIGATION_BAR_WIDGETS = "navigation_bar_widgets";
     private static final String PREF_MENU_ARROWS = "navigation_bar_menu_arrow_keys";
+    private static final String NAVBAR_HIDE_ENABLE = "navbar_hide_enable";
+    private static final String NAVBAR_HIDE_TIMEOUT = "navbar_hide_timeout";
 
 
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
@@ -115,6 +117,8 @@ public class NavigationBar extends SettingsPreferenceFragment implements
     CheckBoxPreference mEnableNavringLong;
     CheckBoxPreference mMenuArrowKeysCheckBox;
     Preference mConfigureWidgets;
+    CheckBoxPreference mNavBarHideEnable;
+    ListPreference mNavBarHideTimeout;
 
     private File customnavImage;
     private File customnavTemp;
@@ -178,6 +182,15 @@ public class NavigationBar extends SettingsPreferenceFragment implements
         mNavBarButtonQty.setOnPreferenceChangeListener(this);
         mNavBarButtonQty.setValue(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.NAVIGATION_BAR_BUTTONS_QTY, 3) + "");
+
+        mNavBarHideEnable = (CheckBoxPreference) findPreference(NAVBAR_HIDE_ENABLE);
+        mNavBarHideEnable.setChecked(Settings.System.getBoolean(getContentResolver(),
+                Settings.System.NAV_HIDE_ENABLE, false));
+
+        mNavBarHideTimeout = (ListPreference) findPreference(NAVBAR_HIDE_TIMEOUT);
+        mNavBarHideTimeout.setOnPreferenceChangeListener(this);
+        mNavBarHideTimeout.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NAV_HIDE_TIMEOUT, 3000) + "");
 
         mEnableNavringLong = (CheckBoxPreference) findPreference("enable_navring_long");
         mEnableNavringLong.setChecked(Settings.System.getBoolean(getContentResolver(),
@@ -310,6 +323,17 @@ public class NavigationBar extends SettingsPreferenceFragment implements
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
             Helpers.restartSystemUI();
             return true;
+        } else if (preference == mNavBarHideEnable) {
+
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.NAV_HIDE_ENABLE,
+                    ((CheckBoxPreference) preference).isChecked());
+
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_SHOW_NOW, !((CheckBoxPreference) preference).isChecked());
+            Helpers.restartSystemUI();
+            refreshSettings();
+            return true;
         } else if (preference == mEnableNavringLong) {
 
             Settings.System.putBoolean(getActivity().getContentResolver(),
@@ -425,6 +449,12 @@ public class NavigationBar extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT,
                     height);
             // showDialog(DIALOG_NAVBAR_HEIGHT_REBOOT);
+            return true;
+        } else if (preference == mNavBarHideTimeout) {
+            int val = Integer.parseInt((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAV_HIDE_TIMEOUT, val);
+            refreshSettings();
             return true;
         } else if (preference == mNavigationBarHeightLandscape) {
             String newVal = (String) newValue;
