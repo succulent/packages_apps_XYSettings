4e495f86b02fe25525e984759e2721794e78dae7
Zaphod <mwilson@acesappraisal.com>
2013-01-28 02:27:00 +0800

    LastApp Toggle [2/2]
    Provides option for new LastApp Action in frameworks.
    Can be used as NavBar or NavRing.
    PS2 : Add icons by Hoolm.
    PS3 : Remove unnecessary dependency
    
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    Change-Id: Icda3a83f6cd8378f57043678c4535dcb8f245eec

--- src/com/xylon/settings/fragments/NavigationBar.java
+++ src/com/xylon/settings/fragments/NavigationBar.java
@@ -801,6 +801,8 @@ public class NavigationBar extends SettingsPreferenceFragment implements
                 return getResources().getDrawable(R.drawable.ic_sysbar_power);
             } else if (uri.equals("**notifications**")) {
                 return getResources().getDrawable(R.drawable.ic_sysbar_notifications);
+            } else if (uri.equals("**lastapp**")) {
+                return getResources().getDrawable(R.drawable.ic_sysbar_lastapp);
             }
         } else {
             try {
@@ -847,6 +849,8 @@ public class NavigationBar extends SettingsPreferenceFragment implements
                 return getResources().getString(R.string.navbar_action_power);
             else if (uri.equals("**notifications**"))
                 return getResources().getString(R.string.navbar_action_notifications);
+            else if (uri.equals("**lastapp**"))
+                return getResources().getString(R.string.navbar_action_lastapp);
             else if (uri.equals("**null**"))
                 return getResources().getString(R.string.navbar_action_none);
             else if (uri.equals("**widgets**"))
